bbdd0afa7f12d6c97dcbc6fdb4eb56fe1c05cc1c
Sol <slereah@gmail.com>
2022-05-11 22:04:17 -0300

    Ejercicio del modulo 2

---
+++ ejercicio-modulo2/index.js
@@ -0,0 +1,8 @@
+const lista = [
+    'Sol', 42, true, new Date(1979, 06, 30),
+{
+    titulo:'eramos unos niños',
+    autor:'patti smith',
+    fecha:new Date(2010, 0, 19),
+    url:'https://www.penguinlibros.com/es/biografias/30717-libro-eramos-unos-ninos-9788426408549'
+} ]
