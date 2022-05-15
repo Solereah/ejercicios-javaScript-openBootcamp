20fc94a7eae0e9276f38c16ed8353e9c05d32a1c
Sol <slereah@gmail.com>
2022-05-15 19:35:03 -0300

    Ejercicio Modulo 5

---
+++ ejercicio-modulo5/index.js
@@ -0,0 +1,8 @@
+
+const alturaCms = 173
+const alturaMts = 1.73
+const peso = 72.4
+const alturaCeil = Math.ceil(alturaMts)
+const pesoFloor = Math.floor(peso)
+let maxValorJs = Number.MAX_VALUE
+const maxValueJs= ((2**1023)+1)
