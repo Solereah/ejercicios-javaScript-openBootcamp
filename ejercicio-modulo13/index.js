dd2cc84da4d75392ee45a5223717d780541071b8
Sol <slereah@gmail.com>
2022-05-26 09:41:35 -0300

    Ejercicio modulo 13

---
+++ ejercicio-modulo13/index.js
@@ -0,0 +1,10 @@
+const nombre = "Sol"
+const apellido = "Lereah"
+const estudiante = nombre.concat("", apellido)
+const estudianteMayus = estudiante.toUpperCase()
+const estudianteMinus = estudiante.toLocaleLowerCase()
+const numeroLetras = estudiante.length
+const primeraLetra = nombre[0]
+const ultimaLetra = apellido.slice(-1)
+const cadenaSinEspacios = estudiante.replace("" , "")
+const incluyeNombre = estudiante.includes(nombre)
