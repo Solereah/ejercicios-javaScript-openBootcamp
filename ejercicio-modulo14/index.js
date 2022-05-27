f9f12c0e136ead945e57c73b0d8f5d7fcb1d3e0c
Sol <slereah@gmail.com>
2022-05-27 11:32:40 -0300

    Ejercicio modulo 14

---
+++ ejercicio-modulo14/index.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("btn")
+
+btn.addEventListener('click',()=>{
+    alert("click en el botón")
+})
+
+$("#jq").click(()=>{
+    console.log("Hola, estoy utilizando jQuery");
+});
